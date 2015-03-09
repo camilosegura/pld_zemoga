b51835283ebb21543541a7fc51cee3d8130e4bca
camilosegura <camilosegurar@gmail.com>
2015-03-08 20:29:56 -0500

    Added first styles for responsive.

--- src/js/main.js
+++ src/js/main.js
@@ -25,4 +25,34 @@ function loadGalleryData(){
 }
 
 
-(loadGalleryData())();
+function buttonCollapser(){
+	var btnMenuColl = document.querySelector(".menu-collapse");
+	var menuFirst = document.querySelector(".menu-first");
+	var hidden = true;
+	var height = 0;
+	
+	height = menuFirst.offsetHeight;
+	menuFirst.style.height = "0";
+
+	console.log("__ " + height);
+	
+	btnMenuColl.addEventListener('click', function(){
+		if(hidden){
+		   menuFirst.style.height = height + "px";           
+           hidden = false;
+		}else{
+			menuFirst.style.height = "0px";
+			hidden = true;
+		}
+
+	});
+}
+
+(function initReady(){
+	loadGalleryData();
+})();
+
+window.onload = function(){
+    buttonCollapser();
+}
+
